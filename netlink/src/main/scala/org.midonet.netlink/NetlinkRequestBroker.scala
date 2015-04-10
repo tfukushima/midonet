55d4f854377abb69e40174537ed706c0a9e052ef
Taku Fukushima <tfukushima@midokura.com>
2015-04-10 11:31:21 +0900

    f lib
    Change-Id: I4d4cbf342404733e633905b61031849181f60097
    Signed-off-by: Taku Fukushima <tfukushima@midokura.com>

--- netlink/src/main/scala/org.midonet.netlink/NetlinkRequestBroker.scala
+++ netlink/src/main/scala/org.midonet.netlink/NetlinkRequestBroker.scala
@@ -270,8 +270,7 @@ final class NetlinkRequestBroker(writer: NetlinkBlockingWriter,
         val obs = getObserver(pos, seq, unhandled)
 
         val `type` = readBuf.getShort(start + NetlinkMessage.NLMSG_TYPE_OFFSET)
-        if (`type` >= NLMessageType.NLMSG_MIN_TYPE &&
-            size >= NetlinkMessage.HEADER_SIZE) {
+        if (`type` >= NLMessageType.NLMSG_MIN_TYPE && size >= headerSize) {
 
             val flags = readBuf.getShort(start + NetlinkMessage.NLMSG_FLAGS_OFFSET)
 
@@ -291,7 +290,9 @@ final class NetlinkRequestBroker(writer: NetlinkBlockingWriter,
         }
 
         freeSequence(pos, seq)
-        obs.onCompleted()
+        if (obs ne notificationObserver) {
+            obs.onCompleted()
+        }
     }
 
     private def getObserver(pos: Int, seq: Int,
